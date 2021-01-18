e119c29e6cef85599a5c20a993d4d135818479cd
ZhuQiyue <zhuqiyue@ufl.edu>
2021-01-19 04:14:34 +0800

    Create a project with empty control system.

---
+++ src/cnt5106C/ControlSystem.java
@@ -0,0 +1,11 @@
+package cnt5106C;
+
+public class ControlSystem {
+	/**
+	 * The main function for every peerProcess.
+	 * @param args an argument from console, which is the peerId of that particular peerProcess
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
